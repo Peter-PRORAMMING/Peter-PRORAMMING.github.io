2f9383f531ac5fdafdc8be7d707ce1781777ed9f
Peter-PRORAMMING <109823789+Peter-PRORAMMING@users.noreply.github.com>
2022-10-07 20:37:21 -0700

    Update boom.js

--- boom.js
+++ boom.js
@@ -1,3 +1,3 @@
 function Boom() {
-    document.body.innerHTML = "BOOOOOOOOOOOOOM!";
+    document.body.innerHTML = '<img src="https://upload.wikimedia.org/wikipedia/commons/9/96/SN_2006gy%2C_NASA_illustration.jpg">';
 }
